20a535a67fd1673de553faa5fbc7e22d6f6abc62
yukihane <yukihane.feather@gmail.com>
2015-09-14 12:01:31 +0900

    Injectしてみる

---
+++ hello-cdi-ejb/src/main/java/com/example/samples/MyCdiImpl1.java
@@ -0,0 +1,10 @@
+package com.example.samples;
+
+public class MyCdiImpl1 implements MyCdi {
+
+    @Override
+    public String hello() {
+        return "Hello MyCdiImpl1";
+    }
+
+}
--- hello-cdi-web/src/main/java/com/example/servlet/HelloServlet.java
+++ hello-cdi-web/src/main/java/com/example/servlet/HelloServlet.java
@@ -3,11 +3,14 @@ package com.example.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.example.samples.MyCdi;
 
 /**
  * Servlet implementation class HelloServlet
@@ -15,6 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/helloservlet")
 public class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+
+    @Inject
+    private MyCdi myCdi;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,8 +35,9 @@ public class HelloServlet extends HttpServlet {
      *      response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String hello = myCdi.hello();
         try (PrintWriter writer = response.getWriter()) {
-            writer.println("<h1>Hello Servlet!!</h1>");
+            writer.println("<h1>" + hello + "</h1>");
         }
     }
 
